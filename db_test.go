f97a72643000d889aaada9e46883aaa152740df0
HokieGeek <andres.f.perez@gmail.com>
2016-11-05 02:17:39 -0400

    Stubbing out the db test

--- db_test.go
+++ db_test.go
@@ -6,8 +6,30 @@ import (
 
 func TestGetSheet(t *testing.T) {
 	t.Skip("TODO")
+
+	// db, err := getSheet("https://docs.google.com/spreadsheets/d/1-U45bMxRE4_n3hKRkTPTWHTkVKC8O3zcSmkjEyYFYOo/pub?output=tsv")
+	// if err != nil {
+	// return nil, err
+	// }
 }
 
 func TestBuildDatabase(t *testing.T) {
 	t.Skip("TODO")
+
+	// typesFilter := make(map[string]struct{})
+	// if len(*teaTypes) > 0 {
+	// for _, typeFilter := range strings.Split(*teaTypes, ",") {
+	// typesFilter[strings.ToLower(typeFilter)] = struct{}{}
+	// }
+	// }
+
+	// db, err := buildDatabase(*stockedFlag, *samplesFlag, typesFilter)
+	// if err != nil {
+	// log.Fatal(err)
+	// }
+
+	// fmt.Printf("%-60s %6s %6s %6s %6s\n", "Name", "Num", "Avg", "Median", "Mode")
+	// for _, tea := range db {
+	// fmt.Printf("%-60s %6d %6d %6d %6d\n", tea.String(), len(tea.Log), tea.Average(), tea.Median(), tea.Mode())
+	// }
 }
