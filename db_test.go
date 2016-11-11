1881a1c35cc3155c85e3f9f90ecf6ba41ea123e0
HokieGeek <andres.f.perez@gmail.com>
2016-11-11 11:30:07 -0500

    Coverage for the Filter type

--- db_test.go
+++ db_test.go
@@ -1,27 +1,66 @@
 package hgtealib
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestNewFilter(t *testing.T) {
-	t.Skip("TODO")
+	if NewFilter() == nil {
+		t.Fatal("Cannot create a Filter object")
+	}
 }
 
 func TestFilterStockedOnly(t *testing.T) {
-	t.Skip("TODO")
+	if NewFilter().StockedOnly().stockedOnly == false {
+		t.Fatal("StockedOnly is not set to true as expected")
+	}
+
+	if NewFilter().stockedOnly != false {
+		t.Fatal("Default value for StockedOnly is not false")
+	}
 }
 
 func TestFilterSamplesOnly(t *testing.T) {
-	t.Skip("TODO")
+	if NewFilter().SamplesOnly().samplesOnly == false {
+		t.Fatal("SamplesOnly is not set to true as expected")
+	}
+
+	if NewFilter().samplesOnly != false {
+		t.Fatal("Default value for SamplesOnly is not false")
+	}
 }
 
 func TestFilterTypes(t *testing.T) {
-	t.Skip("TODO")
+	testTypes := []string{"T1", "T2", "T3"}
+	f := NewFilter().Types(testTypes)
+
+	for _, v := range testTypes {
+		if _, ok := f.types[strings.ToLower(v)]; !ok {
+			t.Fatalf("Did not find vilter type '%s' in Filter types map: %+v", v, f.types)
+		}
+	}
+
+	f = NewFilter().Types([]string{"", "", "", ""})
+	if len(f.types) != 0 {
+		t.Error("Was able to add empty types to the Filter")
+	}
 }
 
 func TestFilterType(t *testing.T) {
-	t.Skip("TODO")
+	testTypes := []string{"T1", "T2", "T3"}
+	f := NewFilter()
+	for _, v := range testTypes {
+
+		f = f.Type(v)
+	}
+
+	for _, v := range testTypes {
+		if _, ok := f.types[strings.ToLower(v)]; !ok {
+			t.Fatalf("Did not find vilter type '%s' in Filter types map: %+v", v, f.types)
+		}
+	}
+
 }
 
 func TestNewHgTeaDb(t *testing.T) {
